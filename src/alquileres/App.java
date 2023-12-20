5879caef3afc8a2e96807141cebf9d5ae053bab1
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-12-20 14:04:34 +0100

    app-loadVehiculos

--- src/alquileres/App.java
+++ src/alquileres/App.java
@@ -41,4 +41,43 @@ public class App {
             }
         }
     }
+    public void loadData() {
+        this.loadVehiculos();
+        this.loadAlquileres();
+        System.out.print("Continuar: ....");
+        this.scan.next();
+    }
+    public void loadVehiculos() {
+        String matricula = ""; String marca=""; String tipo = ""; int km;
+        System.out.println("Carga de vehiculos:");
+        try {
+            Turismo t; Furgoneta f;
+            System.out.println(this.rutavehiculos);
+            FileReader vehiculosjson = new FileReader(this.rutavehiculos);
+            JSONParser parser = new JSONParser();
+            Object objVehiculos = parser.parse(vehiculosjson);
+            JSONArray VehiculosJA = (JSONArray) objVehiculos;
+
+            for (int i=0; i<VehiculosJA.size(); i++){
+
+                JSONObject VehiculoObj = (JSONObject) VehiculosJA.get(i);
+                matricula = (String) VehiculoObj.get("matricula");
+                marca = (String) VehiculoObj.get("marca");
+                tipo = (String) VehiculoObj.get("tipo");
+                km = Integer.parseInt ((String) VehiculoObj.get("km"));
+                //System.out.println(matricula +  marca + tipo +km);
+                if (tipo.equals("T")) {
+                    t = new Turismo(matricula, marca, km);
+                    this.LVehiculos.add(t);
+                }else {
+                    f = new Furgoneta(matricula, marca, km);
+                    this.LVehiculos.add(f);
+                }
+                
+            }
+
+        } catch (Exception e) {
+            System.out.println("eror");
+        }
+    }
 }
